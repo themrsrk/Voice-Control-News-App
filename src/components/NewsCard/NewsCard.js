c5dabfea1e121f6dd58e3fddf9abda6a07d0baad
Shahrukh <themrsrk@gmail.com>
2022-05-23 19:47:09 +0500

    Add articles into Cards

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,8 +1,32 @@
 import React from 'react'
-
-const NewsCard = () => {
+import { Card, CardActionArea, CardActions, Typography, CardContent, CardMedia, Button } from '@material-ui/core'
+import useStyles from './styles.js'
+const NewsCard = ({ articles: { description, publishedAt, source, title, url, urlToImage }, index }) => {
+  const classess = useStyles()
   return (
-    <div>NewsCard</div>
+    <Card className={classess.card}>
+      <CardActionArea href={url} target='_blank'>
+        <CardMedia className={classess.media} image={urlToImage || 'https://media.istockphoto.com/photos/breaking-news-world-news-with-map-backgorund-picture-id1182477852?k=20&m=1182477852&s=612x612&w=0&h=I3wdSzT_5h1y9dHq_YpZ9AqdIKg8epthr8Guva8FkPA='} />
+        <div className={classess.details}>
+          <Typography variant='body2' color='textSecondary' component='h2'>{(new Date(publishedAt)).toDateString()}</Typography>
+          <Typography variant='body2' color='textSecondary' component='h2'>{source.name}</Typography>
+          {/* //variant is just a small text */}
+          {/* Every text in the material ui is the typography  */}
+        </div>
+
+        <Typography className={classess.title} gutterBottom variant='h5'>{title}</Typography>
+        {/* gutterBottom is just like a padding and margin at the bottom  */}
+
+        <CardContent>
+          <Typography variant='body2' color='textSecondary' component='p'>{description}</Typography>
+        </CardContent>
+
+        <CardActions className={classess.cardActions}>
+          <Button size='small' color='primary'>Learn More</Button>
+          <Typography variant='h5' color='textSecondary'>{index + 1}</Typography>
+        </CardActions>
+      </CardActionArea>
+    </Card>
   )
 }
 
--- src/components/NewsCards/NewsCards.jsx
+++ src/components/NewsCards/NewsCards.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Grid, Grow, Typography } from '@material-ui/core'
 import NewsCard from '../NewsCard/NewsCard'
-
+import useStyles from './styles.js'
 
 const NewsCards = ({ articles }) => {
+  const classess = useStyles();
   return (
     <Grow in>
-      <Grid container alignItems='stretch' spacing={3} >
+      <Grid className={classess.container} container alignItems='stretch' spacing={3} >
         {/* spacing means the space between cards */}
-        {articles.map((article, index) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} style={{display: 'flex'}}>
+        {articles.map((articles, index) => (
+          <Grid key={index}item xs={12} sm={6} md={4} lg={3} style={{display: 'flex'}}>
             {/* xs and sm are screen sizes, and 12 and 6 are the values means a grid contain 12 rows so on xtra small screen only card will show, and on small screen 2 cards will be shown */}
             <NewsCard articles={articles} index={index}/>
           </Grid>
